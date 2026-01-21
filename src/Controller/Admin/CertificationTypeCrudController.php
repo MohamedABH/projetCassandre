9ed8e770b498230e062a7fd558450575765f5177
bunri <mabhassine@gmail.com>
2026-01-21 14:54:17 +0100

    created easyAdmin cruds & redirected the admin route to a crud route

---
+++ src/Controller/Admin/CertificationTypeCrudController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\CertificationType;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class CertificationTypeCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return CertificationType::class;
+    }
+
+    /*
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id'),
+            TextField::new('title'),
+            TextEditorField::new('description'),
+        ];
+    }
+    */
+}
--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -7,18 +7,24 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
+use App\Entity\AuditTask;
+use App\Entity\AuditType;
+use App\Entity\Category;
+use App\Entity\CertificationType;
+use App\Entity\Role;
+use App\Entity\Tag;
 
 #[AdminDashboard(routePath: '/admin', routeName: 'admin')]
 class DashboardController extends AbstractDashboardController
 {
     public function index(): Response
     {
-        return parent::index();
+        //return parent::index();
 
         // Option 1. You can make your dashboard redirect to some common page of your backend
         //
         // 1.1) If you have enabled the "pretty URLs" feature:
-        // return $this->redirectToRoute('admin_user_index');
+        return $this->redirectToRoute('admin_category_index');
         //
         // 1.2) Same example but using the "ugly URLs" that were used in previous EasyAdmin versions:
         // $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
@@ -45,6 +51,11 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        yield MenuItem::linkToCrud('AuditTasks', 'fa fa-list', AuditTask::class);
+        yield MenuItem::linkToCrud('AuditTypes', 'fa fa-list', AuditType::class);
+        yield MenuItem::linkToCrud('Categories', 'fa fa-list', Category::class);
+        yield MenuItem::linkToCrud('CertificationTypes', 'fa fa-list', CertificationType::class);
+        yield MenuItem::linkToCrud('Roles', 'fa fa-list', Role::class);
+        yield MenuItem::linkToCrud('Tags', 'fa fa-list', Tag::class);
     }
 }
